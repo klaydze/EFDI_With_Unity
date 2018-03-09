39848a9e09c8f248647f7ca877f80d99be6b1bdb
jessie.p.semana <jessie.p.semana@CIOS-5254399-LX>
2018-03-09 13:57:47 +0800

    1. Add additional UI     - 1 textbox     - 1 numericupdown 2. Add additional code for Save button

--- EFDI/Form1.cs
+++ EFDI/Form1.cs
@@ -1,5 +1,4 @@
-﻿using Accenture.Online.Service.Implementation;
-using EFDI.Domain.Models;
+﻿using EFDI.Domain.Models;
 using EFDI.Services.Contracts;
 using Microsoft.Practices.Unity;
 using System;
@@ -31,34 +30,6 @@ namespace EFDI
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string enc = CryptographicService.CustomEncrypt("patrick.a.de.ocampo@accenture.com", false);
-            string dec = CryptographicService.CustomDecrypt("bkCOOLpHZKji4n%2fjxSBhXaI1jWpFacG%2bW2zjnr3AezF%2f95rpBRgli4vS3sYYYKz2WKSTCWHZy9byViZg7sL4WJiMNGY61zQbUXSMhFxqvOzkQmvRRmnU3hTZTFu2uqwH");
-
-            RecipeDomain recipe = new RecipeDomain()
-            {
-                RecipeName = "Dumplings",
-                CategoryId = 3
-            };
-            //RecipeService.Insert(recipe);
-            //RecipeService.Save();
-
-            //var d = RecipeService.GetEntityById(1);
-            //d.RecipeName = "Rice Toppings";
-            //RecipeService.Update(d);
-            //RecipeService.Save();
-
-
-
-
-
-
-
-
-
-
-
-
-
             //Approach #1
             //IRecipeService service = container.Resolve<IRecipeService>();
             //service.AddRecipe(new Recipe { Name = "Spaghetti", CategoryId = 2 });
@@ -71,6 +42,18 @@ namespace EFDI
         {
             Form2 frm2 = UContainer.Resolve<Form2>();
             frm2.Show();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            RecipeDomain recipe = new RecipeDomain()
+            {
+                RecipeName = textBox1.Text,
+                CategoryId = Convert.ToInt32(numericUpDown1.Value)
+            };
+
+            RecipeService.Insert(recipe);
+            RecipeService.Save();
         }
     }
 }
